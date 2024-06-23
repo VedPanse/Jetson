1511c6f8d71689f2bc3a70e8d66d20c4d495dedf
Ved Panse <vedkpanse@gmail.com>
2024-06-23 16:20:32 +0530

    Added parsing functionality to manifest.json

--- program/program/src/AppLauncher.java
+++
@@ -1,5 +0,0 @@
-public class AppLauncher {
-    public static void main(String[] args) {
-        System.out.println("This class should deal with launching applications.");
-    }
-}
---
+++ program/program/src/Tester.java
@@ -0,0 +1,12 @@
+/**
+ * This class is the main class where all the components are brought together for testing purposes.
+ */
+
+public class Tester {
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser("../manifest.json");
+        String targetPath = parser.getTarget();
+
+        System.out.println(targetPath);
+    }
+}
