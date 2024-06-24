0c881dbb4c394660c0ff5878dc61268ab5efdbe4
Ved Panse <vedkpanse@gmail.com>
2024-06-24 12:38:47 +0530

    added SpringBoot

---
+++ program/program/src/jetson-gui/src/main/java/com/jetson/jetson_gui/JetsonGuiApplication.java
@@ -0,0 +1,13 @@
+package com.jetson.jetson_gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetsonGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetsonGuiApplication.class, args);
+	}
+
+}
---
+++ program/program/src/jetson-gui/src/test/java/com/jetson/jetson_gui/JetsonGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetson.jetson_gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetsonGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
